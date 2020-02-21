1aad2bdb0cd91b0a4f341d42a48a9d722a4a804a
[Braden Niksich] <[abniksi@ilstu.edu]>
2020-02-21 15:08:40 -0600

    first commit

---
+++ src/com/luv2code/springdemo/Basketball.java
@@ -0,0 +1,9 @@
+package com.luv2code.springdemo;
+
+public class Basketball implements Coach{
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Shoot 100 free throws and make 80 of them";
+	}
+}
